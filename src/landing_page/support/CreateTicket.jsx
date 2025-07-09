142c7735edc9add9e55403cfee4b0fe2847dc93d
urshivamthakur <shivamthakur0620@gamil.com>
2025-07-10 00:36:03 +0530

    Support Section Setup

--- src/landing_page/support/CreateTicket.jsx
+++ src/landing_page/support/CreateTicket.jsx
@@ -1,9 +1,162 @@
 import React from "react";
 
 function CreateTicket() {
-    return ( 
-        <h1>CreateTicket</h1>
-     );
+  return (
+    <div className="container">
+      <p className="fs-4 pt-5 pb-3 text-muted">
+        To create a ticket, select a relevant topic
+      </p>
+      <div className="row">
+        <div className="col-4 support-table-style">
+          <table>
+            <tr>
+              <th>
+                <i class="fa-solid fa-circle-plus"></i> &nbsp; Account Opening
+              </th>
+            </tr>
+            <tr>
+              <td>Resident individual</td>
+            </tr>
+            <tr>
+              <td>Minor</td>
+            </tr>
+            <tr>
+              <td>Non Resident Indian (NRI)</td>
+            </tr>
+            <tr>
+              <td>Company, Partnership, HUF and LLP</td>
+            </tr>
+            <tr>
+              <td>Glossary</td>
+            </tr>
+          </table>
+        </div>
+        <div className="col-4 support-table-style">
+          <table>
+            <tr>
+              <th>
+                <i class="fa-solid fa-circle-plus"></i> &nbsp; Your Zerodha
+                Account
+              </th>
+            </tr>
+            <tr>
+              <td>Your Profile</td>
+            </tr>
+            <tr>
+              <td>Account modification</td>
+            </tr>
+            <tr>
+              <td>
+                Client Master Report (CMR) and Depository Participant (DP)
+              </td>
+            </tr>
+            <tr>
+              <td>Nomination</td>
+            </tr>
+            <tr>
+              <td>Transfer and conversion of securities</td>
+            </tr>
+          </table>
+        </div>
+        <div className="col-4 support-table-style">
+          <table>
+            <tr>
+              <th>
+                <i class="fa-solid fa-circle-plus"></i> &nbsp; Kite
+              </th>
+            </tr>
+            <tr>
+              <td>IPO</td>
+            </tr>
+            <tr>
+              <td>Margin Trading Facility (MTF) and Margins</td>
+            </tr>
+            <tr>
+              <td>Charts and orders</td>
+            </tr>
+            <tr>
+              <td>Alerts and Nudges</td>
+            </tr>
+            <tr>
+              <td>General</td>
+            </tr>
+          </table>
+        </div>
+        <div className="col-4 support-table-style">
+          <table>
+            <tr>
+              <th>
+                <i class="fa-solid fa-circle-plus"></i> &nbsp; Kite
+              </th>
+            </tr>
+            <tr>
+              <td>IPO</td>
+            </tr>
+            <tr>
+              <td>Margin Trading Facility (MTF) and Margins</td>
+            </tr>
+            <tr>
+              <td>Charts and orders</td>
+            </tr>
+            <tr>
+              <td>Alerts and Nudges</td>
+            </tr>
+            <tr>
+              <td>General</td>
+            </tr>
+          </table>
+        </div>
+        <div className="col-4 support-table-style">
+          <table>
+            <tr>
+              <th>
+                <i class="fa-solid fa-circle-plus"></i> &nbsp; Kite
+              </th>
+            </tr>
+            <tr>
+              <td>IPO</td>
+            </tr>
+            <tr>
+              <td>Margin Trading Facility (MTF) and Margins</td>
+            </tr>
+            <tr>
+              <td>Charts and orders</td>
+            </tr>
+            <tr>
+              <td>Alerts and Nudges</td>
+            </tr>
+            <tr>
+              <td>General</td>
+            </tr>
+          </table>
+        </div>
+        <div className="col-4 support-table-style">
+          <table>
+            <tr>
+              <th>
+                <i class="fa-solid fa-circle-plus"></i> &nbsp; Kite
+              </th>
+            </tr>
+            <tr>
+              <td>IPO</td>
+            </tr>
+            <tr>
+              <td>Margin Trading Facility (MTF) and Margins</td>
+            </tr>
+            <tr>
+              <td>Charts and orders</td>
+            </tr>
+            <tr>
+              <td>Alerts and Nudges</td>
+            </tr>
+            <tr>
+              <td>General</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- src/landing_page/support/Hero.jsx
+++ src/landing_page/support/Hero.jsx
@@ -1,9 +1,62 @@
 import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <section
+      className="container-fluid 
+    support-hero-style p-5"
+    >
+      <div className="container">
+        <div className="d-flex justify-content-between mx-5 px-4">
+          <h5>Support Portal</h5>
+          <a href="#">Track tickets</a>
+        </div>
+        <div className="row p-4 mx-5 align-items-center">
+          <div className="col-6 py-4 pe-5 ps-0">
+            <h5 className="pe-5 fs-4">
+              Search for an answer or browse help topics to create a ticket
+            </h5>
+            <div className="pt-5 pb-3">
+              <input
+                className="form-control w-100 p-3"
+                placeholder="Eg: how do i activate F&O, why is my order getting rejected ..."
+              ></input>
+            </div>
+            <div className="d-flex pt-2">
+              <p className="pe-4">
+                <a href="#">Track account opening</a>
+              </p>
+              <p>
+                <a href="#">Track segment activation</a>
+              </p>
+            </div>
+            <div className="d-flex">
+              <p className="pe-4">
+                <a href="#">Kite user manual</a>
+              </p>
+              <p>
+                <a href="#">Intraday margins</a>
+              </p>
+            </div>
+          </div>
+          <div className="col-6 py-4 pe-5">
+            <h5 className="pe-5 fs-4">Featured</h5>
+            <ol>
+              <li className="p-3">
+                <a href="#">Quarterly Settlement of Funds - July 2025</a>
+              </li>
+              <li className="p-2">
+                <a href="#">
+                  Exclusion of F&O contracts on 8 securities from August 29,
+                  2025
+                </a>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
